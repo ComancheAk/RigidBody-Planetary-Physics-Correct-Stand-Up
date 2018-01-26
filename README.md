c7726ecfa1a97b78888cabdb431f2e10d38bb922
Dmitry Trifonov <comanche.ak@gmail.com>
2018-01-26 13:22:02 +0300

    keyboard movement

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This demo shows how to make correct objects stand up function for planetary gravity in Godot Engine.
 Requires Godot Engine 3.0 RC3 or higher version.
 
-![Sceenshot](gif2.gif)
+Use Arrow keys or WASD to move the capsule.
+
+![Sceenshot](gif3.gif)
 
 Feel free to contact me if you got questions about this demo.
--- RigidBody.gd
+++ RigidBody.gd
@@ -1,17 +1,45 @@
 extends RigidBody
 var target #Variable for current object Transform
 var gravityCentre #Variable for gravity centre
+var xVelocity
+var zVelocity
 
 
 func _ready():
 	gravityCentre = get_node("../StaticBody").get_transform()
+	zVelocity = 0
+	xVelocity = 0
 	pass
 
 func _integrate_forces(state):
 	target = state.transform
-	print(state.get_total_gravity()) #Information for testing
+	#print(state.get_total_gravity()) #Information for testing
 	state.transform = orthonormalizePlanetary(target,gravityCentre)
-	state.set_linear_velocity((target.basis.x+target.basis.z)+state.get_total_gravity())
+	if (Input.is_action_pressed("ui_up")):
+		zVelocity += 0.1
+	if (Input.is_action_just_released("ui_up")):
+		zVelocity = 0
+	if (Input.is_action_pressed("ui_down")):
+		zVelocity -= 0.1
+	if (Input.is_action_just_released("ui_down")):
+		zVelocity = 0
+	if (zVelocity > 1):
+			zVelocity = 1	
+	if (zVelocity < -1):
+			zVelocity = -1
+	if (Input.is_action_pressed("ui_left")):
+		xVelocity += 0.1
+	if (Input.is_action_just_released("ui_left")):
+		xVelocity = 0
+	if (Input.is_action_pressed("ui_right")):
+		xVelocity -= 0.1
+	if (Input.is_action_just_released("ui_right")):
+		xVelocity = 0
+	if (xVelocity > 1):
+			xVelocity = 1	
+	if (xVelocity < -1):
+			xVelocity = -1
+	state.set_linear_velocity((target.basis.x*xVelocity+target.basis.z*zVelocity)+state.get_total_gravity())
 	pass
 
 func orthonormalizePlanetary(t,p):
